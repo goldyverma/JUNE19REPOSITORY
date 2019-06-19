54dbaa3c28309accb7db81cf279ac8e72bcf393a
Goldy Verma <749947@student.tor.loc>
2019-06-19 15:55:34 -0400

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 //basic way to create an object
-const circle = {
-    radius: 1,
-    location: {
-        x:1,
-        y:1
-    },
+
+/*function createCircle(radius){
+return {
+    radius,
     draw: function(){
         console.log('draw');
     }
 };
+}
+const circle = createCircle(1);
+circle.draw();*/
+
+//consturctor Fuction
+function Circle(radius){
+  
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+    
+}
+
+
+
+const circle1 = new Function('radius',
+`this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }`);
+    const orange = new Circle(1);
+
+    const another = new Circle(1);
