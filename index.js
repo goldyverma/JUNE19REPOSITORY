4c006f727f7d5478b8d19c38f2ecf0d516e5b556
Goldy Verma <749947@student.tor.loc>
2019-06-19 14:08:18 -0400

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+//basic way to create an object
+const circle = {
+    radius: 1,
+    location: {
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+    }
+};
